0fd7c5cca8ead949ef605eebcbbcaaab0a5affec
sisandamthabela <mthabelasisanda@gmail.com>
2025-07-08 22:46:45 +0200

    completed all the js

--- index.js
+++ index.js
@@ -2,23 +2,38 @@ let cardsEl = document.querySelector("#cards-el");
 let sumEl = document.querySelector("#sum-el");
 let message = "";
 let messageEl = document.querySelector("#message-el");
-let firstCard = randomCard();
-let secondCard = randomCard();
-let sum = firstCard + secondCard;
-let isAlive = true;
+let playerEl = document.querySelector("#player-el");
+let sum = 0;
+let cards = [];
+let isAlive = false;
 let hasBlackJack = false;
-let cards = [firstCard, secondCard];
+let player = {
+  name: 'Sisanda',
+  chips: 145
+}
 
 function startGame() {
+  isAlive = true;
+  let firstCard = randomCard();
+  let secondCard = randomCard();
+  sum = firstCard + secondCard;
+  cards = [firstCard, secondCard];
   renderGame();
 }
 
 function randomCard() {
   let randomNumber = Math.floor(Math.random() * 13);
-  return randomNumber;
+  if (randomNumber === 1) {
+    return 11;
+  } else if (randomNumber > 10) {
+    return 10;
+  } else {
+    return randomNumber;
+  }
 }
 
 function renderGame() {
+  playerEl.innerHTML = player.name + ': R' + player.chips
   cardsEl.innerHTML = "Cards: ";
   sumEl.innerHTML = "Sum: " + sum;
   for (let i = 0; i < cards.length; i++) {
@@ -37,9 +52,11 @@ function renderGame() {
 }
 
 function drawCard() {
-  let card = randomCard();
-  cards.push(card);
-  sum += card;
-  sumEl.innerHTML = "Sum: " + sum;
-  renderGame();
+  if (hasBlackJack === false && isAlive === true) {
+    let card = randomCard();
+    cards.push(card);
+    sum += card;
+    sumEl.innerHTML = "Sum: " + sum;
+    renderGame();
+  }
 }
